0a3e3c3526236e3419f8273aad6fd3506a16128d
Aditya Inapurapu <adityaii@gmail.com>
2018-11-18 13:24:52 -0800

    Edited for zadd compatibility

--- primeNumbers.py
+++ primeNumbers.py
@@ -4,18 +4,23 @@ import sys
 import scipy
 import redis
 import requests
+import os
 
 def main():
-	r = redis.StrictRedis(host="172.17.0.2", port=6379)
-	current_prime = str(r.get('current_prime'))
+	r = redis.StrictRedis(host="primeredis", port=6379)
+	# r = redis.StrictRedis(host="0.0.0.0", port=6379)
+	current_prime = r.get('current_prime')
 	i = 2
+	if (not current_prime):
+		current_prime = "2"
 	if (int(current_prime) > 0):
 		i = current_prime
 	counter = 1
+	print(current_prime)
 	while (i > 1):
 		if (isPrime(int(i))):
-			print(str(i))
-			r.zadd('primes', counter, i)
+			print(i)
+			r.zadd('primes', { int(counter): int(i)} )
 			r.set('current_prime', i)
 			postToDashing(i)
 			counter = int(counter) + 1
@@ -23,10 +28,12 @@ def main():
 	r.close()
 
 def postToDashing(prime):
+	token = os.environ.get('AUTH_TOKEN')
 	url = 'https://iaditya.herokuapp.com/widgets/prime'
-	payload = '{ "auth_token": "YOUR_AUTH_TOKEN", "title": %s }' % (prime) 
+	payload = '{ "auth_token": "%s" , "title": %s }' % (token, prime) 
 	headers = '{content-type: application/json}'
 	response = requests.post(url, data=payload)
+	print(response)
 
 def isPrime(x):
     if x > 1:
